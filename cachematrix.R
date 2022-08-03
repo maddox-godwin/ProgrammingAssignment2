58c413c7835ab4d6dc4393228c4b4675c5922700
maddox-godwin <109624731+maddox-godwin@users.noreply.github.com>
2022-08-03 08:16:27 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions parallel the example used for means, 
+## but using matrices and the solve() function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(a) {
+                x <<- a
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <- solve
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
